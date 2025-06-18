50241020075a80f30940fa9e6fcd3011fa634592
Sufang Lai <laisg@mail.uc.edu>
2025-06-18 10:11:39 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 8.9;
+        double num2 = 9.8;
+
+        System.out.println("The total is " + (num1 + num2));
+
+
+    }
+}
+
